6f3c8b9dd199d1f900556d956830e5006593a45e
Qitian Liao <liaoqitian1024@gmail.com>
2020-06-14 23:59:16 -0400

    bug fixes

--- Berkeley Python's Flying Circus/server.py
+++ Berkeley Python's Flying Circus/server.py
@@ -5,7 +5,7 @@ def generate(data):
     # Sample a random starting point and a corresponding ending point of R
     x1 = random.randint(0, 100)
     y1 = random.randint(x1 + 10, x1 + 20)
-    R = [x1, y1]
+    R = [x for x in range(x1, y1 + 1)]
     # Sample a random starting point and a corresponding ending point of list to be queried.
     x2 = random.randint(2, 6)
     y2 = random.randint(x2 + 2, x2+ 4)
@@ -17,24 +17,24 @@ def generate(data):
     data['params']['y2'] = y2
 
     # Compute the solution of the three parts. 
-    data['params']['solution'] = R[x2, y2]
+    data['params']['solution'] = R[x2: y2]
     # Build the confounding choices. 
-    data['params']['i0'] = R[x2, y2 + 1]
-    data['params']['i1'] = R[x2, y2 - 1]
+    data['params']['i0'] = R[x2: y2 + 1]
+    data['params']['i1'] = R[x2: y2 - 1]
 
-    data['params']['i2'] = R[x2 + 1, y2 + 1]
-    data['params']['i3'] = R[x2 - 1, y2 - 1]
+    data['params']['i2'] = R[x2 + 1: y2 + 1]
+    data['params']['i3'] = R[x2 - 1: y2 - 1]
 
-    data['params']['i4'] = R[x2 + 1, y2]
-    data['params']['i5'] = R[x2 - 1, y2]
+    data['params']['i4'] = R[x2 + 1: y2]
+    data['params']['i5'] = R[x2 - 1: y2]
 
-    data['params']['i6'] = [x2, y2]
+    data['params']['i6'] = [x for x in range(x2, y2)]
 
-    data['params']['i7'] = [x2, y2 + 1]
-    data['params']['i8'] = [x2, y2 - 1]
+    data['params']['i7'] = [x for x in range(x2, y2 + 1)]
+    data['params']['i8'] = [x for x in range(x2, y2 - 1)]
 
-    data['params']['i9'] = [x2 + 1, y2 + 1]
-    data['params']['i10'] = [x2 - 1, y2 - 1]
+    data['params']['i9'] = [x for x in range (x2 + 1, y2 + 1)]
+    data['params']['i10'] = [x for x in range(x2 - 1, y2 - 1)]
 
-    data['params']['i11'] = [x2 + 1, y2]
-    data['params']['i12'] = [x2 - 1, y2]
+    data['params']['i11'] = [x for x in range(x2 + 1, y2)]
+    data['params']['i12'] = [x for x in range(x2 - 1, y2)]
