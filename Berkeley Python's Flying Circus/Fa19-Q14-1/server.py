a4dd5b38ee004203376d60e6577a28a35c4dbf63
Qitian Liao <liaoqitian1024@gmail.com>
2020-06-16 14:43:26 -0400

    variable name change

--- Berkeley Python's Flying Circus/Fa19-Q14-1/server.py
+++ Berkeley Python's Flying Circus/Fa19-Q14-1/server.py
@@ -1,40 +1,43 @@
+import prairielearn as pl
 import random, copy, math
 
 def generate(data):
 
     # Sample a random starting point and a corresponding ending point of R
-    x1 = random.randint(0, 100)
-    y1 = random.randint(x1 + 10, x1 + 20)
-    R = [x for x in range(x1, y1 + 1)]
+    start = random.randint(0, 100)
+    end = random.randint(start + 10, start + 20)
+    R = [x for x in range(start, end + 1)]
+
     # Sample a random starting point and a corresponding ending point of list to be queried.
-    x2 = random.randint(2, 4)
-    y2 = random.randint(x2 + 2, x2+ 4)
+    start_index = random.randint(2, 4)
+    end_index = random.randint(start_index + 2, start_index+ 4)
 
     # Put the four parameters into data['params']
-    data['params']['x1'] = x1
-    data['params']['y1'] = y1
-    data['params']['x2'] = x2
-    data['params']['y2'] = y2
+    data['params']['start'] = start
+    data['params']['end'] = end
+    data['params']['start_index'] = start_index
+    data['params']['end_index'] = end_index
 
     # Compute the solution. 
-    data['params']['solution'] = R[x2: y2]
+    data['params']['solution'] = R[start_index: end_index]
+    
     # Build the confounding choices. 
-    data['params']['i0'] = R[x2: y2 + 1]
-    data['params']['i1'] = R[x2: y2 - 1]
+    data['params']['i0'] = R[start_index: end_index + 1]
+    data['params']['i1'] = R[start_index: end_index - 1]
 
-    data['params']['i2'] = R[x2 + 1: y2 + 1]
-    data['params']['i3'] = R[x2 - 1: y2 - 1]
+    data['params']['i2'] = R[start_index + 1: end_index + 1]
+    data['params']['i3'] = R[start_index - 1: end_index - 1]
 
-    data['params']['i4'] = R[x2 + 1: y2]
-    data['params']['i5'] = R[x2 - 1: y2]
+    data['params']['i4'] = R[start_index + 1: end_index]
+    data['params']['i5'] = R[start_index - 1: end_index]
 
-    data['params']['i6'] = [x for x in range(x2, y2)]
+    data['params']['i6'] = [x for x in range(start_index, end_index)]
 
-    data['params']['i7'] = [x for x in range(x2, y2 + 1)]
-    data['params']['i8'] = [x for x in range(x2, y2 - 1)]
+    data['params']['i7'] = [x for x in range(start_index, end_index + 1)]
+    data['params']['i8'] = [x for x in range(start_index, end_index - 1)]
 
-    data['params']['i9'] = [x for x in range (x2 + 1, y2 + 1)]
-    data['params']['i10'] = [x for x in range(x2 - 1, y2 - 1)]
+    data['params']['i9'] = [x for x in range(start_index + 1, end_index + 1)]
+    data['params']['i10'] = [x for x in range(start_index - 1, end_index - 1)]
 
-    data['params']['i11'] = [x for x in range(x2 + 1, y2)]
-    data['params']['i12'] = [x for x in range(x2 - 1, y2)]
+    data['params']['i11'] = [x for x in range(start_index + 1, end_index)]
+    data['params']['i12'] = [x for x in range(start_index - 1, end_index)]
