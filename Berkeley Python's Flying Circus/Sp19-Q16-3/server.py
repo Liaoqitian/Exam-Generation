d8eb43b8fe667a962aabcdcf3c4d0315ece4a662
Qitian Liao <liaoqitian1024@gmail.com>
2020-06-16 11:36:04 -0400

    bug fix and name change

--- Berkeley Python's Flying Circus/Sp19-Q16-3/server.py
+++ Berkeley Python's Flying Circus/Sp19-Q16-3/server.py
@@ -8,7 +8,7 @@ def generate(data):
 
     # Sample the starting and ending point
     a = random.randint(1, 3)
-    b = random.randint(a + 1, a + 3)
+    b = random.randint(a + 1, len(elem) - 1)
 
     # Compute the solution. 
     # if b > len(elem):
@@ -17,12 +17,6 @@ def generate(data):
     solution = elem[a:b]
 
     # Build the confounding choices. 
-    # if solution == "Error": 
-    #     i0 = elem[a : b - 1]
-    #     i1 = elem[a : b - 2]
-    #     i2 = elem[a + 1: b - 1]
-    #     i3 = elem[a - 1: b - 1]
-    #     i4 = 'None of these'
     if b - a == 1:
         i0 = elem[a : b - 1]
         i1 = 'Error'
