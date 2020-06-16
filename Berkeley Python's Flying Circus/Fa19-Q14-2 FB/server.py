5dc2932327d475ce428d56777d57e5221fba0232
Qitian Liao <liaoqitian1024@gmail.com>
2020-06-16 19:49:25 -0400

    q14-2 bug fix

---
+++ Berkeley Python's Flying Circus/Fa19-Q14-2 FB/server.py
@@ -0,0 +1,28 @@
+import prairielearn as pl
+import random, copy, math
+
+def generate(data):
+
+    # Sample a list of random length between 3 and 5. 
+    length = random.randint(3, 6)
+
+    # Sample random elements in (0, 1000) to fill the list. 
+    A = [0] * length
+    copy = [0] * length
+    copy = random.sample(range(0, 1000), length)
+    for i in range(length):   
+        A[i] = str(copy[i])
+
+    # Set up the question. 
+    index = random.randint(1, len(A) - 2)
+    adder = str(random.randint(1, 5))
+    
+    # Compute the correct solution.
+    solution = A[index] + adder
+
+    # Store the parameters.
+    A = str(A).replace("'", '"')
+    data['params']['A'] = A
+    data['params']['index'] = index
+    data['params']['adder'] = adder 
+    data['correct_answers']['solution'] = solution
