88159303a493487b6645a66937c282e3cafa819f
Qitian Liao <liaoqitian1024@gmail.com>
2020-06-17 00:18:35 -0400

    added

--- Berkeley Python's Flying Circus/Fa19-Q14-2 FB/server.py
+++ Berkeley Python's Flying Circus/Fa19-Q14-2 FB/server.py
@@ -1,6 +1,12 @@
 import prairielearn as pl
 import random, copy, math
 
+def enquote_string(s):
+    if type(s) == str:
+        return '"' + s + '"'
+    else:
+        return s
+
 def generate(data):
 
     # Sample a list of random length between 3 and 5. 
@@ -10,7 +16,7 @@ def generate(data):
     A = [0] * length
     copy = [0] * length
     copy = random.sample(range(0, 1000), length)
-    for i in range(length):   
+    for i in range(length): 
         A[i] = str(copy[i])
 
     # Set up the question. 
@@ -18,8 +24,10 @@ def generate(data):
     adder = str(random.randint(1, 5))
     
     # Compute the correct solution.
+    
     solution = A[index] + adder
-    solution = str([solution]).replace("[","").replace("]","").replace("'",'"')
+    data['params']['raw_solution'] = solution
+    solution = str([solution]).replace("[","").replace("]","")
     
     # Store the parameters.
     A = str(A).replace("'", '"')
@@ -27,3 +35,17 @@ def generate(data):
     data['params']['index'] = index
     data['params']['adder'] = adder 
     data['correct_answers']['solution'] = solution
+
+def grade(data):
+    if data['score'] == 0: 
+        if data["submitted_answers"]["solution"] == data['correct_answers']['solution'].replace("'",'"'):
+            data["partial_scores"]["score"] = 1
+            data['score'] = 1
+            data["feedback"]["solution"] = "you got this correct!"
+        elif data["submitted_answers"]["solution"] == data['params']['raw_solution']:
+            data["partial_scores"]["score"] = 1
+            data['score'] = 1
+            data["feedback"]["solution"] = "Remember the quotation marks next time!"
+        else:
+            # data["partial_scores"]["solution"] = 0
+            data["feedback"]["solution"] = "you got this wrong, sorry"
