3b4ca93490a8e4ccf20e7a474e788c3a991de5d3
Qitian Liao <liaoqitian1024@gmail.com>
2020-06-24 23:09:57 -0400

    finally gradable

--- Berkeley Python's Flying Circus/Python-Palindrome/server.py
+++ Berkeley Python's Flying Circus/Python-Palindrome/server.py
@@ -1,7 +1,10 @@
 import prairielearn as pl
 import pandas as pd
 def generate(data):    
-    data['params']['names_for_user'] = []
+    data['params']['names_for_user'] = [
+        {'name': 'all_but_first_of', 'description': 'Function to remove the first character of a string', 'type': 'python function'},
+        {'name': 'all_but_last_of', 'description': 'Function to remove the last character of a string', 'type': 'python function'}
+    ]
     data['params']['names_from_user'] = [
         {'name': 'isPalindrome', 'description': 'Function to determine whether a string is a palindrome', 'type': 'python function'}
     ]
