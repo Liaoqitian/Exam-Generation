e6e91237bf10608a6dc535a72120304e73895185
Qitian Liao <liaoqitian1024@gmail.com>
2020-06-29 14:35:31 -0400

    new test case for palindrome

--- Berkeley Python's Flying Circus/Python-Palindrome-Family/tests/test.py
+++ Berkeley Python's Flying Circus/Python-Palindrome-Family/tests/test.py
@@ -7,35 +7,25 @@ import numpy.random
 
 
 class Test(PLTestCase):
-    @points(0.5)
-    @name('Check checkPalindrome()')
+    @points(2)
+    # Basic Test Suites
+    @name('Basic Tests')
     def test_0(self):
-        user_val = Feedback.call_user(self.st.checkPalindrome, '')
-        if Feedback.check_scalar("checkPalindrome('')", self.ref.checkPalindrome(''), user_val):
-            Feedback.set_score(1)
-        else:
-            Feedback.set_score(0)
-
-    @points(0.5)
-    @name('Check checkPalindrome(a)')
-    def test_1(self):
-        user_val = Feedback.call_user(self.st.checkPalindrome, 'a')
-        if Feedback.check_scalar("checkPalindrome('a')", self.ref.checkPalindrome('a'), user_val):
-            Feedback.set_score(1)
-        else:
-            Feedback.set_score(0)
-
-    @points(1)
-    @name('Check checkPalindrome(peak)')
-    def test_2(self):
-        user_val = Feedback.call_user(self.st.checkPalindrome, 'peak')
-        if Feedback.check_scalar("checkPalindrome('peak')", self.ref.checkPalindrome('peak'), user_val):
-            Feedback.set_score(1)
-        else:
-            Feedback.set_score(0)
+        points = 0
+        num_tests = 3
+        test_strings = ['', 'a', 'peak'] 
+        for s in test_strings:
+            correct_val = self.ref.checkPalindrome(s)
+            user_val = Feedback.call_user(self.st.checkPalindrome, s)
+            if Feedback.check_scalar(f"checkPalindrome({s})", correct_val, user_val):
+                points += 1
+        if points < 3: 
+            points = 0
+        Feedback.set_score(points / num_tests)
 
     @points(8)
-    @name('Check several strings')
+    # Advanced Test Suites
+    @name('Advanced Tests')
     def test_3(self):
         points = 0
         num_tests = 8
