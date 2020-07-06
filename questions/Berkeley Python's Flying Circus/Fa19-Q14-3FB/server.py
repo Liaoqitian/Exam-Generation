cebe01c0b6b1054493c063546e819f3a82fb06c2
Qitian Liao <liaoqitian1024@gmail.com>
2020-07-06 00:46:01 -0400

    alpha version python7

--- questions/Berkeley Python's Flying Circus/Fa19-Q14-3FB/server.py
+++ questions/Berkeley Python's Flying Circus/Fa19-Q14-3FB/server.py
@@ -27,6 +27,10 @@ def generate(data):
     # The resulting function call would look like: reduce(compose, [multiplication_function, str, function_one, function_two, multiplication_function])
     function_list = [double, triple, plus1, plus2, times2, times3]
     multiplication_function = function_list[random.randint(0, 1)] 
+    if multiplication_function == double:
+        data['params']['multiplication_function'] = 'double'
+    else: 
+        data['params']['multiplication_function'] = 'triple'
     selected_function_index = [random.randint(0, 1), random.randint(2, 3), random.randint(4, 5)]
     random_index = random.sample(range(3), 3)
     function_one = function_list[selected_function_index[random_index[0]]]
@@ -47,44 +51,62 @@ def generate(data):
     # Store the corresponding texts 
     if function_one == double: 
         data['params']['text_one'] = "def double(x): return x + x"
+        data['params']['function_one'] = "double"
     elif function_one == triple: 
         data['params']['text_one'] = "def triple(x): return x + x + x"
+        data['params']['function_one'] = "triple"
     elif function_one == plus1:
         data['params']['text_one'] = "def plus1(x): return x + 1" 
+        data['params']['function_one'] = "plus1"
     elif function_one == plus2:
         data['params']['text_one'] = "def plus2(x): return x + 2"
+        data['params']['function_one'] = "plus2"
     elif function_one == times2:
         data['params']['text_one'] = "def times2(x): return x * 2"
+        data['params']['function_one'] = "times2"
     else: 
         data['params']['text_one'] = "def times3(x): return x * 3"
+        data['params']['function_one'] = "times3"
     
     if function_two == double: 
         data['params']['text_two'] = "def double(x): return x + x"
+        data['params']['function_two'] = "double"
     elif function_two == triple: 
         data['params']['text_two'] = "def triple(x): return x + x + x"
+        data['params']['function_two'] = "triple"
     elif function_two == plus1:
         data['params']['text_two'] = "def plus1(x): return x + 1" 
+        data['params']['function_two'] = "plus1"
     elif function_two == plus2:
         data['params']['text_two'] = "def plus2(x): return x + 2"
+        data['params']['function_two'] = "plus2"
     elif function_two == times2:
         data['params']['text_two'] = "def times2(x): return x * 2"
+        data['params']['function_two'] = "times2"
     else: 
         data['params']['text_two'] = "def times3(x): return x * 3"
+        data['params']['function_two'] = "times3"
 
     if function_three == double: 
         data['params']['text_three'] = "def double(x): return x + x"
+        data['params']['function_three'] = "double"
     elif function_three == triple: 
         data['params']['text_three'] = "def triple(x): return x + x + x"
+        data['params']['function_three'] = "triple"
     elif function_three == plus1:
         data['params']['text_three'] = "def plus1(x): return x + 1" 
+        data['params']['function_three'] = "plus1"
     elif function_three == plus2:
         data['params']['text_three'] = "def plus2(x): return x + 2"
+        data['params']['function_three'] = "plus2"
     elif function_three == times2:
         data['params']['text_three'] = "def times2(x): return x * 2"
+        data['params']['function_three'] = "times2"
     else: 
         data['params']['text_three'] = "def times3(x): return x * 3"
+        data['params']['function_three'] = "times3"
     
-    data['params']['frankenstein'] = str(reduce(compose, [multiplication_function, str, function_one, function_two, function_three]))
+    # data['params']['frankenstein'] = "reduce(compose, [)" + multiplication_function + "str, " + function_one + ", " + function_two + ", " + function_three + "])"
 
     data['correct_answers']['solution'] = solution
     data['params']['returned_value'] = returned_value
