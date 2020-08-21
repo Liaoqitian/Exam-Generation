26300df92b2470ba55775a91b33dc67e9458f7d2
Qitian Liao <liaoqitian1024@gmail.com>
2020-08-21 11:08:14 -0400

    nim game almost complete

---
+++ questions/python/nim-game/server.py
@@ -0,0 +1,6 @@
+import random 
+
+def generate(data):
+    data["correct_answers"]["solution_one"] = "return 1"
+    data["correct_answers"]["solution_two"] = ""
+    data["correct_answers"]["solution_three"] = "return sum(temp)"
