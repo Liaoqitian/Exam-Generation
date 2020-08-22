6f7a42ff33b18774c9cceacf0e9ca5661fc443ed
Qitian Liao <liaoqitian1024@gmail.com>
2020-08-22 12:42:32 -0400

    dict-key-values

---
+++ questions/python/dictionary-keys-values/README.md
@@ -0,0 +1,6 @@
+# Title
+> Description
+## Table of Contents
+## Examples
+## Solutions
+## Contact liaoqitian1024@gmail.com or find Qitian Liao on Slack for questions
---
+++ questions/python/dictionary-keys-values/server.py
@@ -0,0 +1,26 @@
+import random 
+
+def generate(data):
+    adjectives = ["big", "small", "pretty", "ugly", "red", "blue", "tall", "short"]
+    nowns = ["house", "car", "tree", "table", "chair", "fork", "bird", "window"]
+    key_one = adjectives[random.randint(0, 7)]
+    key_two = adjectives[random.randint(0, 7)]
+    count_one = random.randint(0, 9)
+    count_two = random.randint(0, 9)
+    d = {}
+    d[key_one] = count_one
+    d[key_two] = count_two
+    key_solution = d.keys()
+    value_solution = d.values()
+
+    data["params"]["d"] = str(d)
+    data["correct_answers"]["key_solution"] = str(key_solution)
+    data["correct_answers"]["value_solution"] = str(value_solution)
+
+def grade(data): 
+    if data["partial_scores"]["key_solution"]["score"] != 1: 
+        if data["submitted_answers"]["key_solution"] == data['correct_answers']['key_solution'].replace("'",'"'):
+            data["partial_scores"]["key_solution"]["score"] = 1
+            data["score"] += 0.5
+
+    
