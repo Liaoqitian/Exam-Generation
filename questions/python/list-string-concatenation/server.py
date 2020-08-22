3c2416d6db8e571092133f3baa09ede473e7fb6b
Qitian Liao <liaoqitian1024@gmail.com>
2020-08-22 12:22:08 -0400

    list-string-concat

---
+++ questions/python/list-string-concatenation/server.py
@@ -0,0 +1,26 @@
+import random 
+
+def generate(data):
+    list_one = random.sample(range(0, 5), random.randint(1, 2))
+    list_two = random.sample(range(6, 10), random.randint(1, 2))
+    list_solution = list_one + list_two
+    data["params"]["list_one"] = str(list_one)
+    data["params"]["list_two"] = str(list_two)
+    data["correct_answers"]["list_solution"] = str(list_solution)
+
+    adjectives = ["big", "small", "pretty", "ugly", "red", "blue", "tall", "short"]
+    nowns = ["house", "car", "tree", "table", "chair", "fork", "bird", "window"]
+    string_one = adjectives[random.randint(0, 7)]
+    string_two = nowns[random.randint(0, 7)]
+    string_solution = string_one + string_two
+    string_solution = str([string_solution]).replace("[","").replace("]","")
+    data["params"]["string_one"] = string_one
+    data["params"]["string_two"] = string_two
+    data["correct_answers"]["string_solution"] = string_solution.replace("'",'"')
+
+def grade(data):
+    if data["submitted_answers"]["string_solution"] == data['correct_answers']['string_solution'].replace('"',"'"):
+        data["partial_scores"]["string_solution"]["score"] = 1
+        data["score"] += 0.5
+
+    
---
+++ questions/question-template.py
@@ -0,0 +1,65 @@
+import sys
+import os
+
+"""
+This function creates all files and subfolders for a new QG.
+Enter your questions QUID such as 'snap/iteration/new_question'. 
+Make sure all parent directories exist beforehand 
+('snap' & 'iteration' would already exist)
+"""
+def question_template(quid):
+    os.chdir('questions/python')
+    split_dir = quid.split("/")
+    for folder in split_dir:
+        if os.path.isdir(folder):
+            os.chdir(folder)
+        else:
+            os.mkdir(folder)
+            os.chdir(folder)
+
+    server = open('server.py', 'w')
+    question = open('question.html', 'w')
+    info = open('info.json', 'w')
+    readme = open('README.md', 'w')
+    os.mkdir('clientFilesquestion')
+    os.mkdir('serverFilesquestion')
+
+    server.write("""import random 
+def generate(data):
+    return data
+    """)
+    question.write("""<html>
+    <head>
+        <style>
+            pre {
+                font-size: 110%;
+            }
+            code {
+                color: black;
+                padding: 2px;
+                font-size: 110%;
+            }
+        </style>
+        <link rel="stylesheet" href="./styles.css">
+    </head>
+<body>
+<pl-question-panel>
+</pl-question-panel>
+""")
+    info.write("""{
+    "uuid": "REPLACE",
+    "title": "",
+    "topic": "",
+    "tags": ["liaoqitian", "final", "blank", "easy", "formative", "berkeley", "alpha", "Fa14Q12"],
+    "type": "v3"
+}
+""")
+    readme.write("""# Title
+> Description
+## Table of Contents
+## Examples
+## Solutions
+## Contact liaoqitian1024@gmail.com or find Qitian Liao on Slack for questions
+""")
+
+question_template('list-concatenation')
