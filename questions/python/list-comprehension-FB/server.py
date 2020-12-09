d6c2fbd97d909f74e9d1a4005c000675017d62c6
Qitian Liao <liaoqitian1024@gmail.com>
2020-12-09 01:22:45 -0500

    list comprehension fb

--- questions/python/list-comprehension-FB/server.py
+++ questions/python/list-comprehension-FB/server.py
@@ -2,30 +2,32 @@ import prairielearn as pl
 import random, copy, math
 
 def generate(data): 
-    # operations map: 0 - add, 1 - minus, 2 - multiply
-    operation_key = random.randint(0, 2)
-    operator_number = random.randint(2, 3)
+    # map operations map: 0 - add, 1 - minus, 2 - multiply
+    map_operation_key = random.randint(0, 2)
+
+    map_number = random.randint(2, 3)
     range_number = random.randint(7, 10)
     modulo_number = random.randint(2, 4)
     remainder_number = random.randint(0, modulo_number - 1)
-    if operation_key == 0: 
-        operation = "+"
-        outcome = [x + operator_number for x in range(range_number) if x % modulo_number != remainder_number]
-    elif operation_key == 1: 
-        operation = "-"
-        outcome = [x - operator_number for x in range(range_number) if x % modulo_number != remainder_number]
-    elif operation_key == 2:
-        operation = "*"
-        outcome = [x * operator_number for x in range(range_number) if x % modulo_number != remainder_number]
-
-    data["params"]["operation_key"] = operation_key
-    # data["params"]["operator_number"] = operator_number 
+    
+    if map_operation_key == 0: 
+        map_operation = "+"
+        outcome = [x + map_number for x in range(range_number) if x % modulo_number != remainder_number]
+    elif map_operation_key == 1: 
+        map_operation = "-"
+        outcome = [x - map_number for x in range(range_number) if x % modulo_number != remainder_number]
+    elif map_operation_key == 2:
+        map_operation = "*"
+        outcome = [x * map_number for x in range(range_number) if x % modulo_number != remainder_number]
+        
+    data["params"]["operation_key"] = map_operation_key
     data["params"]["range_number"] = range_number
-    # data["params"]["modulo_number"] = modulo_number
     data["params"]["remainder_number"] = remainder_number
-    # data["params"]["operation"] = operation
-
     data["params"]["outcome"] = str(outcome)
-    data["correct_answers"]["solution_one"] = operation
-    data["correct_answers"]["solution_two"] = str(operator_number)
-    data["correct_answers"]["solution_three"] = str(modulo_number)
+    data["params"]["remainder_number"] = str(remainder_number)
+
+    data["correct_answers"]["map_operation"] = map_operation
+    data["correct_answers"]["map_number"] = str(map_number)
+    data["correct_answers"]["modulo_number"] = str(modulo_number)
+    
+        
