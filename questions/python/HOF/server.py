ed8f906a1a75ed75a3569ceea08ac2e98ac3b0b2
Qitian Liao <liaoqitian1024@gmail.com>
2020-12-10 00:09:53 -0500

    HOF

---
+++ questions/python/HOF/server.py
@@ -0,0 +1,8 @@
+import prairielearn as pl
+import random, copy, math
+from functools import reduce
+
+def generate(data): 
+    data["correct_answers"]["solution_one"] = 4
+    data["correct_answers"]["solution_two"] = 6
+    
